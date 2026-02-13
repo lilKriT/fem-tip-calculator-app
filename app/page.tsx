b3f0008240eec28583c94edf7fbc34bc61cd27ff
lilKriT <lilKriTwebDev@gmail.com>
2026-02-13 20:00:42 +0100

    Added new files, content etc.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <p>Bill</p>
+
+      <p>Select Tip %</p>
+      <p>5%</p>
+      <p>10%</p>
+      <p>15%</p>
+      <p>25%</p>
+      <p>50%</p>
+      <p>Custom</p>
+
+      <p>Number of People</p>
+
+      <p>Tip Amount / person</p>
+
+      <p>Total / person</p>
+
+      <p>Reset</p>
+    </div>
+  );
 }
